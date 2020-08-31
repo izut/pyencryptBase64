abc23055d635c037337cd01c9ff231367cf601f8
izut <52826259+izut@users.noreply.github.com>
2020-08-31 17:01:05 +0800

    Update base64crypt.py

--- base64crypt.py
+++ base64crypt.py
@@ -1,7 +1,10 @@
 import base64
 import random
 
+# Don't Edit CH85!!
 CH85 = '!#$%&()*+-0123456789;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ^_`abcdefghijklmnopqrstuvwxyz{|}~'
+
+# Make your own TOKEN_CH85
 TOKEN_CH85 = '&*+-167;?ACDIKMNOUWXYZ^`acdekmtwxy{~S@<_q>nL%V8vzj}Q3uJTb!$|hg4s#9FG52(po=HB)lifRErP0'
 
 def disorder(s):
@@ -14,12 +17,12 @@ def disorder(s):
         ls.append(ls.pop(p))
     return ''.join(ls)
 
-def encode(v: str)->str:
+def b64encode(v: str)->str:
     # 加密
     d = dict(zip(list(CH85), list(TOKEN_CH85)))
     return ''.join([d[s] for s in str(base64.b85encode(bytes(v, encoding='utf8')), encoding='utf8')])
 
-def decode(v: str)->str:
+def b64decode(v: str)->str:
     # 解密
     d = dict(zip(list(TOKEN_CH85), list(CH85)))
     return str(base64.b85decode(bytes(''.join([d[s] for s in v]), encoding='utf8')), encoding='utf8')
